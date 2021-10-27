9cbc0a5113e143051c8e72fb3c9977221e010e70
demostudent15 <demostudent15@firstdraft.com>
2021-10-27 18:04:26 +0000

    Starting point

--- app/controllers/babel_controller.rb
+++ app/controllers/babel_controller.rb
@@ -1,18 +1,15 @@
 class BabelController < ApplicationController
   def translate_results
     @user_text = params.fetch("original_text")
+    @target_lang = params.fetch("target_language", "es")
+    @recipient_number = params.fetch("send_to", false)
 
-    require "google/cloud/translate"
-
-    gt_client = Google::Cloud::Translate.new({ :version => :v2 })
-
-    @translated_text = translation = gt_client.translate(@user_text, { :to => "es" })
+    # Translate into target language and SMS (if number provided).
 
     render({ :template => "babel_templates/translate_results.html.erb" })
   end
 
   def translate_form
-
     render({ :template => "babel_templates/translate_form.html.erb" })
   end
 end
